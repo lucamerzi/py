1e5676fe17822794c4ed1993e78cef98ce411d69
lucamerzi <luca.merzetti@gmail.com>
2018-04-13 15:04:36 +0200

    done

--- Preprocessing_aaa.py
+++ Preprocessing_aaa.py
@@ -40,7 +40,7 @@ def date_converter(date_as_string):
 
 FileName=''
 
-data_acheteurs=pd.read_csv('G:/My Drive/BU DATA/Clients - Prospects/AAA/DASHBOARD OBSERVATOIRE/AAA-Data-Processing/Raw Data/'+FileName+'.csv',encoding='latin-1',sep=';',index_col=False)
+data_acheteurs=pd.read_csv('G:/My Drive/BU DATA/Clients - Prospects/AAA/Dashboard_Automotive_Insights/AAA-Data-Processing/Raw Data/'+FileName+'.csv',encoding='latin-1',sep=';',index_col=False)
 data_acheteurs['DATE']=data_acheteurs.DATE.apply(lambda x: date_converter(x))
 data_acheteurs['TYPE_LOCAT_PRF']=data_acheteurs['TYPE_LOCAT_PRF'].replace(' ', 'TRADITIONNEL')
 data_acheteurs['SEXE']=data_acheteurs['SEXE'].replace('F', 'Femme')
@@ -49,11 +49,11 @@ data_acheteurs['YEAR']=data_acheteurs.DATE.apply(lambda x: x[:4])
 
 data_clean=data_acheteurs[(data_acheteurs.AGE_PROPRIETAIRE>=18) &  (data_acheteurs.AGE_PROPRIETAIRE<=90) & (data_acheteurs.SEXE!=' ')]
 
-data_clean.to_csv('G:/My Drive/BU DATA/Clients - Prospects/AAA/DASHBOARD OBSERVATOIRE/AAA-Data-Processing/Processed Data/SD_'+FileName+'.csv',index=False)
+data_clean.to_csv('G:/My Drive/BU DATA/Clients - Prospects/AAA/Dashboard_Automotive_Insights/AAA-Data-Processing/Processed Data/SD_'+FileName+'.csv',index=False)
 
 ######################################################### STEP 2: PUSH IN BIGQUERY TABLE
 
-osquery='bq load --noreplace --skip_leading_rows 1 "AAADataPool.OBSA_data_age_volume_" "G:/My Drive/BU DATA/Clients - Prospects/AAA/DASHBOARD OBSERVATOIRE/AAA-Data-Processing/Processed Data/SD_'+FileName+'.csv" "G:/My Drive/BU DATA/Clients - Prospects/AAA/DASHBOARD OBSERVATOIRE/AAA-Data-Processing/Schema/SD.json'
+osquery='bq load --noreplace --skip_leading_rows 1 "AAADataPool.OBSA_data_age_volume_" "G:/My Drive/BU DATA/Clients - Prospects/AAA/Dashboard_Automotive_Insights/AAA-Data-Processing/Processed Data/SD_'+FileName+'.csv" "G:/My Drive/BU DATA/Clients - Prospects/AAA/Dashboard_Automotive_Insights/AAA-Data-Processing/Schema/SD.json'
 
 os.system(osquery)
 
@@ -66,7 +66,7 @@ os.system(osquery)
 ######################################################### STEP 1: PROCESS FILE & EXPORT TO CSV
 
 FileName=''
-data_modeles=pd.read_csv('G:/My Drive/BU DATA/Clients - Prospects/AAA/DASHBOARD OBSERVATOIRE/AAA-Data-Processing/Raw Data/'+FileName+'.csv',encoding='latin-1',sep=';',index_col=False)
+data_modeles=pd.read_csv('G:/My Drive/BU DATA/Clients - Prospects/AAA/Dashboard_Automotive_Insights/AAA-Data-Processing/Raw Data/'+FileName+'.csv',encoding='latin-1',sep=';',index_col=False)
 
 data_clean=data_modeles[~data_modeles.DATE.isin(['Ann.@2001', 'Ann.@2002'])].copy()
 
@@ -74,10 +74,10 @@ data_clean['DATE']=data_clean.DATE.apply(lambda x: date_converter(x))
 
 data_clean['YEAR']=data_clean.DATE.apply(lambda x: x[:4])
 
-data_clean.to_csv('G:/My Drive/BU DATA/Clients - Prospects/AAA/DASHBOARD OBSERVATOIRE/AAA-Data-Processing/Processed Data/Cars_'+FileName+'.csv',index=False)
+data_clean.to_csv('G:/My Drive/BU DATA/Clients - Prospects/AAA/Dashboard_Automotive_Insights/AAA-Data-Processing/Processed Data/Cars_'+FileName+'.csv',index=False)
 
 ######################################################### STEP 2: PUSH IN BIGQUERY TABLE
 
-osquery='bq load --noreplace --skip_leading_rows 1 "AAADataPool.OBSA_data_modele_volume_" "G:/My Drive/BU DATA/Clients - Prospects/AAA/DASHBOARD OBSERVATOIRE/AAA-Data-Processing/Processed Data/Cars_'+FileName+'.csv" "G:/My Drive/BU DATA/Clients - Prospects/AAA/DASHBOARD OBSERVATOIRE/AAA-Data-Processing/Schema/Car.json'
+osquery='bq load --noreplace --skip_leading_rows 1 "AAADataPool.OBSA_data_modele_volume_" "G:/My Drive/BU DATA/Clients - Prospects/AAA/Dashboard_Automotive_Insights/AAA-Data-Processing/Processed Data/Cars_'+FileName+'.csv" "G:/My Drive/BU DATA/Clients - Prospects/AAA/Dashboard_Automotive_Insights/AAA-Data-Processing/Schema/Car.json'
 
 os.system(osquery)
